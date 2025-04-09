7ce668e1e7b408dc58681220c50badf531eff7cf
Brooklynn Stevenson <brooksteven580@gmail.com>
2025-04-08 20:03:06 -0400

    updated files

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const path = require('path')
+const port = 3001;
 
-//ejs view engine
-app.set('views',path.join(_direname,'./views'))
-app.set('view engine','ejs')
 
-app.use(express.static('./styles'))
 
-const fs = require('fs');
-
-app.engine("perscholas", (filePath, options, callback) => {
-    fs.readFile(filePath, (err, content) => {
-        if (err) return callback(err);
-
-        const rendered = content
-        .toString()
-        .replaceAll('#title#', `${options.title}`)
-        .replace('#content#', `${options.content}`)
-      return callback(null, rendered)
-    })
-})
-
-//route for home page
-app.get('/',(req,res) => {
-    res.render("index", {port})
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 })
